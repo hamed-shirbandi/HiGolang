1d7bc5401344652006b68d88360fa8b9d3662c28
hamed shirbandi <hamed.shirbandi@gmail.com>
2021-11-22 03:35:07 +0330

    add some tests

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	// "fmt"
-
-	//
 	"codeblock.ir/helloGoLang/mathRestApi"
 )
 
 func main() {
 
-	// var firstNum string
-	// var secondNum string
-
-	// fmt.Println("enter num 1 :")
-	// fmt.Scanln(&firstNum)
-	// fmt.Println("enter num 2 :")
-	// fmt.Scanln(&secondNum)
-
-	// fmt.Println("the sum is :")
-
-	// var a = mathOperations.ParseAndSum(firstNum, secondNum)
-	// fmt.Println(a)
-
+	//handle http requests
 	mathRestApi.HandleRequests()
 }
